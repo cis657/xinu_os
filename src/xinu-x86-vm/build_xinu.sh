559737ffb6a9655e8d3dca4d28b9716f8d6d6254
RobLFord <r.ford1223@gmail.com>
2019-04-11 20:54:36 -0400

    removing duplicate files

--- src/xinu-x86-vm/build_xinu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-make clean -C compile >/dev/null;make -C compile | tail;./compile/upload.sh
--- src/xinu-x86-vm/compile/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo 'Copying Xinu to TFTP'
-cp ./compile/xinu /srv/tftp/xinu.boot
-echo 'Successfully copied Xinu to TFTP'
