2628cc1a294d91667b809f1325f0a6733b4a30fe
RobLFord <r.ford1223@gmail.com>
2019-04-12 10:48:56 -0400

    updating with code from the lab solution

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -1,9 +1,16 @@
-/*  main.c  - main */
+
+
+#define ORIGINAL 0
+#define PART_III 0
+#define PART_IV 1
+
+#if ORIGINAL
+*main.c - main * /
 
 #include <xinu.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+			  int main(int argc, char **argv)
 {
 	uint32 retval;
 
@@ -12,12 +19,49 @@ int main(int argc, char **argv)
 	/* Wait for shell to exit and recreate it */
 
 	recvclr();
-	while (TRUE) {
+	while (TRUE)
+	{
 		retval = receive();
 		kprintf("\n\n\rMain process recreating shell\n\n\r");
 		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
 	}
-	while (1);
+	while (1)
+		;
 
 	return OK;
 }
+#elif PART_III
+/* hello.c - main*/
+#include <xinu.h>
+/* main - just say hello, then exit */
+void main(void)
+{
+	printf("Hello, world!\n");
+}
+#elif PART_IV
+
+// Example straight from the text book
+/* ex3.c - main, sndch */
+#include <xinu.h>
+void sndch(char);
+/*------------------------------------------------------------------------
+* main - Example of 2 processes executing the same code concurrently
+*------------------------------------------------------------------------
+*/
+void main(void)
+{
+	resume(create(sndch, 1024, 40, "send A", 1, 'A'));
+	resume(create(sndch, 1024, 20, "send B", 1, 'B'));
+}
+/*------------------------------------------------------------------------
+* sndch - Output a character on a serial device indefinitely
+*------------------------------------------------------------------------
+*/
+void sndch(
+	char ch /* The character to emit continuously */
+)
+{
+	while (1)
+		putc(CONSOLE, ch);
+}
+#endif
